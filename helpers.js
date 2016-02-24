fd72846a772ecd257c9f94630ead42aa65eecd39
Adrian <adrian.gawinek@wp.pl>
2016-02-24 23:45:49 +0100

    old version

--- helpers.js
+++ helpers.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var crypto = require('crypto');
 var async = require("async");
 
-var execVoidFunInLoopAsync = function(loopCount, fun, params, callback){	
+var execVoidFunInLoopAsync = function(loopCount, fun, params, callback){
 	var counter = 0;
 	loopCount = loopCount >= 0 ? loopCount : 0;
 	params = params ? params : [];
@@ -22,11 +22,11 @@ var execVoidFunInLoopAsync = function(loopCount, fun, params, callback){
 
 var calculateDigest = function(algorithm, doc, encoding){
 	var digest;
-	var encoding = encoding ? encoding : 'hex';	
+	var encoding = encoding ? encoding : 'hex';
 	try {
 		var shasum = crypto.createHash(algorithm);
 		shasum.update(doc);
-		digest = shasum.digest(encoding); 
+		digest = shasum.digest(encoding);
 	}catch(e){
 		console.log(e);
 	}
@@ -47,7 +47,7 @@ var calculateMultiDigest = function(doc, algorithms, callback, loopCount){
 		var queueForAlgorithms = async.queue(function(method, callback){
 				console.log("processing " + method);
 				console.time(method);
-				execVoidFunInLoopAsync(loopCount, 
+				execVoidFunInLoopAsync(loopCount,
 					function(){
 						calculateDigest(method,doc);
 					},
@@ -55,34 +55,34 @@ var calculateMultiDigest = function(doc, algorithms, callback, loopCount){
 					function() {
 						console.timeEnd(method);
 						callback(null, method + ": " + calculateDigest(method,doc));
-					});						
-			}, algorithms.length);			
-		var digests = [];		
-		for(var i = 0; i < algorithms.length; i++){	
-			var method = algorithms[i];		
+					});
+			}, algorithms.length);
+		var digests = [];
+		for(var i = 0; i < algorithms.length; i++){
+			var method = algorithms[i];
 			console.log( method + " queued for processing...");
 			queueForAlgorithms.push(method, function(err, digest){
 				if(err) {
-					digests.push(err);			
+					digests.push(err);
 				} else {
 					digests.push(digest);
 				}
 			});
-							
+
 		}
-		queueForAlgorithms.drain = function(){		
-			callback(null, digests);			
+		queueForAlgorithms.drain = function(){
+			callback(null, digests);
 		};
 	}
 }
 
-var hmac = function(algorithm, key, text, encoding) {	
+var hmac = function(algorithm, key, text, encoding) {
 	var hmac = crypto.createHmac(algorithm, key);
 	return hmac.update(new Buffer(text)).digest(encoding);
 }
 
 var encode = function(obj, encoding) {
-	var stringifyObj = JSON.stringify(obj);	
+	var stringifyObj = JSON.stringify(obj);
 	return new Buffer(stringifyObj).toString(encoding);
 }
 
@@ -92,13 +92,13 @@ var readJSONFile = function(fileName){
 		throw new Error("unable to open file: " + fileName);
 	}
 	var data = fs.readFileSync(fileName, {encoding:'utf8'});
-	console.log(data);
+	console.log("JSON is read\n" + data);
 	var object = JSON.parse(data);
-	return object;	  	
+	return object;
 }
 
 exports.calculateDigest = calculateDigest; //= function(algorithm, text, encoding) e.g. calculateMultiDigest("md5", "some text", "hex")
 exports.calculateMultiDigest = calculateMultiDigest; //= function(doc, algorithms, callback, loopCount)
-exports.hmac = hmac; // = function(algorithm, key, text, encoding) 
+exports.hmac = hmac; // = function(algorithm, key, text, encoding)
 exports.encode = encode; //encode = function(obj, encoding)
 exports.readJSONFile = readJSONFile; // = function(fileName)
